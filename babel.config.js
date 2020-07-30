5426721be5f9db1366a1f909f497a19685b0e2de
Kirill Beldyaga <kirill.beldyaga@gmail.com>
2020-07-30 13:02:02 +0300

    babel

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,21 @@
 module.exports = {
-  presets: [
-    '@vue/app',
-  ],
+    presets: [
+        [
+            '@vue/app',
+            {
+                useBuiltIns: 'entry',
+            },
+        ],
+        [
+            '@babel/preset-env',
+            {
+                useBuiltIns: 'entry',
+            },
+        ],
+    ],
+    plugins: [
+        '@babel/plugin-transform-modules-commonjs',
+        '@babel/plugin-transform-runtime',
+        '@babel/plugin-transform-async-to-generator',
+    ],
 };
